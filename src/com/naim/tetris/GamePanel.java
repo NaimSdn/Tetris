b354afd3a6325269378305f31b017b470fe62abb
NaimSdn <saadoune.naim.pro@gmail.com>
2024-03-04 14:56:49 +0100

    First Window

---
+++ src/com/naim/tetris/GamePanel.java
@@ -0,0 +1,56 @@
+package com.naim.tetris;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel implements Runnable {
+
+    public static final int WIDTH = 1280;
+    public static final int HEIGHT = 720;
+
+    final int FPS = 60;
+    Thread gameThread;
+    PlayManager playManager;
+
+    public GamePanel(){
+        this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        this.setBackground(Color.black);
+        this.setLayout(null);
+        playManager = new PlayManager();
+    }
+
+    public void launchGame(){
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+        double drawInterval = (double) 1000000000 / FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+
+        while(gameThread != null){
+            currentTime = System.nanoTime();
+            delta += (currentTime - lastTime) / drawInterval;
+            lastTime = currentTime;
+
+            if(delta >= 1){
+                update();
+                repaint();
+                delta--;
+            }
+        }
+    }
+
+    private void update(){
+        playManager.update();
+    }
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D)g;
+        playManager.draw(g2);
+    }
+}
